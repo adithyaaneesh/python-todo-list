da7247453e71e91066d58b7b6f593ec6f14ab495
adithyaaneesh <adithyaaneesh29@gmail.com>
2025-10-27 12:04:49 +0530

    minor changes

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMidddeleware',
+    'whitenoise.middleware.WhiteNoiseMiddeleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
